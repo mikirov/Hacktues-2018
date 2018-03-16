657fb4050b5dba2b531461bbfcb197dc86eab159
tonipelovski <Tonipelovski@gmail.com>
2018-03-16 09:58:27 +0200

    obsticles_class

---
+++ src/main/obsticles_class.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Obsticle(pygame.sprite.Sprite):
+    def __init___(self):
+        self.sprite.Sprite.__init__(self)
+        self.rect = pygame.Rect(0,0, 16, 16)
+        self.image = pygame.image.load('src/resources/rect_obst.png')
+        self.obst_hp = 100
