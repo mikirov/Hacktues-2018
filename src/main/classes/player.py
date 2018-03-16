cb9c64c4a3a5f41f3b0ea7daa429caa3abbdc2b7
shimonchick <shimonchick@gmail.com>
2018-03-16 13:55:59 +0200

    fixed player

--- src/main/classes/player.py
+++ src/main/classes/player.py
@@ -5,6 +5,16 @@ class Player(GameObject):
     def __init__(self, start_x, start_y, image_filepath=None, speed=10, hp=100):
         super().__init__(start_x, start_y, image_filepath, speed)
         self.hp = hp
+    
+    def move(self, direction):
+        if direction == Direction.UP:
+            self.y -= self.speed
+        elif direction == Direction.DOWN:
+            self.y += self.speed
+        elif direction == Direction.LEFT:
+            self.x -= self.speed
+        elif direction == Direction.RIGHT:
+            self.x += self.speed
 
     def shoot(self):
         pass 
