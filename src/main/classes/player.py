43f1eae33dd60c20fa0c4dd23190183ceb8ae702
Petar Nikolov <penikolov23@gmail.com>
2018-03-16 09:43:07 +0200

    Add speed to Player class

--- src/main/classes/player.py
+++ src/main/classes/player.py
@@ -2,6 +2,7 @@ from game_object import GameObject
 
 
 class Player(GameObject):
-    def __init__(self, start_x, start_y):
+    def __init__(self, start_x, start_y, speed=1):
         super().__init__(start_x, start_y)
+        self.speed = 1
 
