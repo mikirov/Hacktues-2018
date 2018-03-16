10aadc6d19e447ca554bc05cf3f4b3c2bdb07375
shimonchick <shimonchick@gmail.com>
2018-03-16 17:24:04 +0200

    Delete Projectile objects that exit the screen

--- src/main/classes/abilities.py
+++ src/main/classes/abilities.py
@@ -1,4 +1,3 @@
-from ..helpers.image_getter import get_image
 from .game_object import GameObject
 
 
@@ -50,4 +49,4 @@ class Build(Ability):
             stone.x -= 5
         if facing == 3: #RIGHT
             stone.x += 5
-        screen.blit(get_image(self.image_filepath), (stone.x, stone.y))
+        #screen.blit(get_image(self.image_filepath), (stone.x, stone.y))
