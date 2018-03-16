a9cb8348c0bdc367e78e56efce87ca9aac0fef04
Denis <denis.barzanov2002@gmail.com>
2018-03-16 15:32:18 +0200

    game_object.py resource changed

--- src/main/classes/projectile.py
+++ src/main/classes/projectile.py
@@ -5,4 +5,3 @@ class Projectile(GameObject):
     def __init__(self, start_x, start_y, image_filepath=None, speed=10, damage=20):
         super().__init__(start_x, start_y, image_filepath, speed)
         self.damage = damage
-
