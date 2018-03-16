9d7d60a5f4544acf4114ca41d7224d5196fd6119
Petar Nikolov <penikolov23@gmail.com>
2018-03-16 09:52:48 +0200

    Add move methods to GameObject

--- src/main/classes/game_object.py
+++ src/main/classes/game_object.py
@@ -3,4 +3,16 @@ class GameObject:
         self.x = start_x
         self.y = start_y
         self.speed = speed
-    
+
+    def move_up(self, step):
+        self.y -= step * speed
+
+    def move_down(self, step):
+        self.y += step * speed
+
+    def move_right(self, step):
+        self.x += step * speed
+
+    def move_left(self, step):
+        self.x -= step * speed
+
