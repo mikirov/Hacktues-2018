9dc0133de36bf4791ce1f095698e79879e2afde9
Denis <denis.barzanov2002@gmail.com>
2018-03-16 12:58:24 +0200

    Added changes to the obstacle class

--- src/main/classes/obsticle.py
+++ src/main/classes/obsticle.py
@@ -1,8 +1,10 @@
 import pygame
 
-class Obsticle(pygame.sprite.Sprite):
+from pygame.sprite import Sprite
+
+class Obsticle(Sprite):
     def __init___(self):
-        self.sprite.Sprite.__init__(self)
+        super().__init__(self)
         self.rect = pygame.Rect(0,0, 16, 16)
         self.image = pygame.image.load('src/resources/rect_obst.png')
         self.obst_hp = 100
