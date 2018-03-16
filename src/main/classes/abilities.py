15da25bc3da6f627e4599ca8ab9215a2066851d2
blackcat <stef4oben88@gmail.com>
2018-03-16 15:40:45 +0200

    Abilities, Player and heal

--- src/main/classes/abilities.py
+++ src/main/classes/abilities.py
@@ -4,8 +4,12 @@ class Ability:
         self.name = name
         self.ab_type = ab_type
         self.cool = cooldown
+        self.func = None
     def __str__(self):
         return self.name
+
+    def __call__(self, *args, **kwargs):
+        return self.func
 
     def hp_change(self,pl, amount):
         pl.hp = amount
--- src/main/classes/player.py
+++ src/main/classes/player.py
@@ -1,12 +1,16 @@
 from .direction import Direction
 from .game_object import GameObject
-
+from .abilities import Ability
+import random
 
 class Player(GameObject):
     def __init__(self, start_x, start_y, image_filepath=None, speed=10, hp=100):
         super().__init__(start_x, start_y, image_filepath, speed)
         self.hp = hp
         self.current_facing = None
+        self.ability = Ability("Heal", "passive", 5)
+        self.ability.amount = random.randint(1,10)
+        self.ability.func = ab.hp_change(self, self.hp+ab.amount)
     
     def move(self, direction):
         if direction == Direction.UP and self.y > 0:
@@ -18,6 +22,9 @@ class Player(GameObject):
         elif direction == Direction.RIGHT and self.x < 800:
             self.x += self.speed
         self.current_facing = direction
+
+    def heal(self):
+        ability()
 
     def shoot(self):
         pass 
