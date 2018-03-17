ab90ae95d39b3ea0ace7b0b7899aa7a412c81899
blackcat <stef4oben88@gmail.com>
2018-03-17 14:31:44 +0200

    Build in main fix

--- src/main/main.py
+++ src/main/main.py
@@ -63,7 +63,7 @@ class App:
                 self.projectiles.append(projectile)
             elif event.code == C1_RIGHT2:
                 if player1.special_ability is Build:
-                    self.objects.append(player1.build(player1)) # todo what da Fu
+                    self.objects.append(player1.build()) # todo what da Fu
         elif player == 2:
             if event.code == C2_BUTTON_DOWN:
                 player2.move(Direction.DOWN)
