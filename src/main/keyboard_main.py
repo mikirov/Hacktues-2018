1e4782ca37bd61c4347b276c7d936cfed9a11f3e
shimonchick <shimonchick@gmail.com>
2018-04-26 00:18:37 +0300

    Configure other controller

--- src/main/keyboard_main.py
+++ src/main/keyboard_main.py
@@ -9,8 +9,7 @@ from helpers.device_finder import find_device
 from classes.player import Player
 from classes.stone import Stone
 from classes.direction import Direction
-from config.controller_config import *
-# from config.keyboard_config import *
+from config.keyboard_config import *
 from config.game_config import *
 
 
@@ -44,14 +43,14 @@ class Game:
             C1_RIGHT1: False,
             C1_RIGHT2: False,
 
-            C2_BUTTON_DOWN * 2: False,
-            C2_BUTTON_UP * 2: False,
-            C2_BUTTON_LEFT * 2: False,
-            C2_BUTTON_RIGHT * 2: False,
-            C2_LEFT1 * 2: False,
-            C2_LEFT2 * 2: False,
-            C2_RIGHT1 * 2: False,
-            C2_RIGHT2 * 2: False,
+            C2_BUTTON_DOWN: False,
+            C2_BUTTON_UP: False,
+            C2_BUTTON_LEFT: False,
+            C2_BUTTON_RIGHT: False,
+            C2_LEFT1: False,
+            C2_LEFT2: False,
+            C2_RIGHT1: False,
+            C2_RIGHT2: False,
         }
 
     def on_init(self):
@@ -61,15 +60,14 @@ class Game:
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, FONT_SIZE)
 
-    def on_event(self, event, current_player):
-        print(event)
+    def on_event(self, event):
         if event.code == EXIT_BUTTON:
             self._running = False
         else:
             if event.value == 0:
-                self.events[event.code * current_player] = False
+                self.events[event.code] = False
             else:
-                self.events[event.code * current_player] = True
+                self.events[event.code] = True
 
     def process_input(self):
         # Player 1
@@ -98,25 +96,25 @@ class Game:
             self.game_objects.append(stone)
 
         # Player 2
-        if self.events[C2_BUTTON_DOWN * 2]:
+        if self.events[C2_BUTTON_DOWN]:
             self.player2.move(Direction.DOWN, self.game_objects)
-        if self.events[C2_BUTTON_UP * 2]:
+        if self.events[C2_BUTTON_UP]:
             self.player2.move(Direction.UP, self.game_objects)
-        if self.events[C2_BUTTON_LEFT * 2]:
+        if self.events[C2_BUTTON_LEFT]:
             self.player2.move(Direction.LEFT, self.game_objects)
-        if self.events[C2_BUTTON_RIGHT * 2]:
+        if self.events[C2_BUTTON_RIGHT]:
             self.player2.move(Direction.RIGHT, self.game_objects)
-        if self.events[C2_LEFT1 * 2]:
+        if self.events[C2_LEFT1]:
             self.player2.heal()
-        if self.events[C2_LEFT2 * 2]:
+        if self.events[C2_LEFT2]:
             self.player2.hit(self.player1)
-        if self.events[C2_RIGHT1 * 2]:
+        if self.events[C2_RIGHT1]:
             current_time = time()
             if current_time - self.player2.last_projectile_fired_at >= COOLDOWN:
                 projectile = self.player2.shoot(get_image(PROJECTILE_IMAGE))
                 self.projectiles.append(projectile)
                 self.player2.last_projectile_fired_at = current_time
-        if self.events[C2_RIGHT2 * 2]:
+        if self.events[C2_RIGHT2]:
             stone = self.player2.build()
             stone.image = get_image(stone.image)
             stone.make_hitbox()
@@ -185,9 +183,10 @@ class Game:
             r, w, x = select(devs, [], [], 1/FRAMERATE)
             for fd in r:
                 for event in devs[fd].read():
-                    player = 1 if devs[fd] is gamepad1 else 2
-                    if event.type == evdev.ecodes.EV_KEY or event.type == 3:
-                        self.on_event(event, player)
+                    if event.type == evdev.ecodes.EV_KEY:
+                        # player = 1 if devs[fd] is gamepad1 else 2
+                        if event.type == evdev.ecodes.EV_KEY:
+                            self.on_event(event)
             self.process_input()
             self.loop()
             self.render()
@@ -209,12 +208,7 @@ class Game:
 
 def main():
     global gamepad1, gamepad2
-
-    gamepad1 = find_device(GAMEPAD_NAME_1)
-    gamepad2 = find_device(GAMEPAD_NAME_2)
-    print(gamepad1)
-    print(gamepad2)
-    # gamepad1 = gamepad2 = keyboard
+    gamepad1 = gamepad2 = keyboard
 
     game = Game(True) # give True to enable hitbox drawing
     game.execute()  
